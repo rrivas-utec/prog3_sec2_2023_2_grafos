8051ac5895b8823c4cf7e2ec60984aa48fb7574c
rrivas <rrivas@utec.edu.pe>
2023-11-23 22:06:45 -0500

    Update grafo

--- graph.h
+++ graph.h
@@ -7,25 +7,77 @@
 #include <vector>
 #include <forward_list>
 #include <unordered_map>
+#include <limits>
 
-template <typename KeyType, typename ValueType, typename WeightType>
-class graph {
-    using IndexType = size_t;
-    using ItemType = std::pair<IndexType, ValueType>;
-    using AdjancentType = std::pair<IndexType, WeightType>;
-    std::unordered_map<KeyType, ItemType> vertices;
-    std::vector<std::forward_list<AdjancentType>> adjacent_list;
+namespace utec {
+    template<typename KeyType, typename ValueType, typename WeightType>
+    class graph {
+        // implemento los tipos
+        using ItemType = std::pair<size_t, ValueType>;
+        using AdjacentType = std::pair<size_t, WeightType>;
+        using AdjacentListType = std::forward_list<AdjacentType>;
+        // Implementación de atributos
+        std::unordered_map<KeyType, ItemType> vertices;
+        std::vector<AdjacentListType> buckets;
+    public:
+        graph() = default;
 
-public:
-    graph() = default;
-    bool push_vertex(KeyType key, ValueType value) {
-        if (vertices.find(key) == std::end(vertices)) {
-            vertices.try_emplace(key, size(adjacent_list), value);
-            adjacent_list.emplace_back();
+        bool push_vertex(KeyType key, ValueType value) {
+//            if (vertices.insert({key, {std::size(buckets), value}}).second) {
+//                buckets.emplace();
+//            }
+            if (vertices.try_emplace(key, std::size(buckets), value).second) {
+                buckets.emplace_back();
+                return true;
+            }
+            return false;
         }
-    }
-    bool push_edge(std::tuple<KeyType, KeyType, WeightType> edge)
 
-};
+        void push_edge(std::tuple<KeyType, KeyType, WeightType> edge) {
+//            const auto& [u, v, w] = edge;
+            const auto& u = std::get<0>(edge);
+            const auto& v = std::get<1>(edge);
+            const auto& w = std::get<2>(edge);
+            // Validación de los vertices
+            if (!vertices.contains(u)) throw std::runtime_error("Error");
+            if (!vertices.contains(v)) throw std::runtime_error("Error");
+            // Buscamos los indices
+            auto idx_u = vertices[u].first;
+            auto idx_v = vertices[v].first;
+            // Insertar un nuevo adjacente
+            buckets[idx_u].emplace_front(idx_v, w);
+            buckets[idx_v].emplace_front(idx_u, w);
+        }
+
+        ValueType get_vertex(KeyType key) {
+            if (!vertices.contains(key)) throw std::runtime_error("Error");
+            return vertices[key].second;
+        }
+
+        WeightType get_edge(KeyType u, KeyType v) {
+            // Validacion
+            if (!vertices.contains(u)) throw std::runtime_error("Error");
+            if (!vertices.contains(v)) throw std::runtime_error("Error");
+            // Buscamos los indices
+            auto idx_u = vertices[u].first;
+            auto idx_v = vertices[v].first;
+            auto it = std::find_if(
+                    std::begin(buckets[idx_u]), std::end(buckets[idx_u]),
+                [idx_v](auto adj) { return adj.first == idx_v; });
+            if (it != std::end(buckets[idx_u])) {
+                return it->second;
+            }
+            else {
+                return std::numeric_limits<WeightType>::infinity();
+            }
+        }
+
+        template<typename UnaryFunction>
+        void bfs(KeyType key, UnaryFunction fn) {
+
+        }
+
+    };
+}
 
 #endif //PROG3_SEC2_2023_2_GRAFOS_GRAPH_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "graph.h"
+
+void ejemplo_1() {
+    // Crear el grafo
+    utec::graph<char, int, int> g1;
+    // Crear los vertices
+    g1.push_vertex('A', 10);
+    g1.push_vertex('B', 20);
+    g1.push_vertex('C', 50);
+    g1.push_vertex('D', 30);
+    // Crear las aristas
+    g1.push_edge({'A', 'B', 2});
+    g1.push_edge({'A', 'C', 4});
+    g1.push_edge({'B', 'C', 7});
+    g1.push_edge({'C', 'D', 5});
+    // Leer el valor de un vertice
+    std::cout << g1.get_vertex('A') << std::endl;        // 10
+    // Leer el peso de una arista
+    std::cout << g1.get_edge('B', 'C') << std::endl;    // 7
+
+//    g1.bfs('A', [](auto ))
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ejemplo_1();
     return 0;
 }
